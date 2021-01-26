01a036e3a6dcf0ab15fd98972f816f37d422904f
adityasah19 <72205327+adityasah19@users.noreply.github.com>
2021-01-26 19:17:28 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master/MatterJSBoilerPlate-master/sketch.js
@@ -0,0 +1,40 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 400);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.  
+	ground=new Ground(400,380,800,10)
+	paper=new Paper(410,380,100,100)
+	dustbin=new Dustbin(600,380,50,50)
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  dustbin.display()
+  ground.display()
+  paper.display()
+  
+ 
+}
+
+
